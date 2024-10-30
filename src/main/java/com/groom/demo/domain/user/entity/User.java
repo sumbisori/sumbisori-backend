df9bf80e3bec7ef080c03ac0df57274aa641a81b
junest66 <chlwndks333@gmail.com>
2024-10-31 03:28:04 +0900

    feat: User entity 추가

---
+++ src/main/java/com/groom/demo/domain/user/entity/User.java
@@ -0,0 +1,51 @@
+package com.groom.demo.domain.user.entity;
+
+import com.groom.demo.domain.base.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(columnDefinition = "varchar(20)")
+    private String providerType;
+
+    @Column(nullable = false)
+    private String providerId;
+
+    @Column(columnDefinition = "varchar(20)")
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Getter
+    @AllArgsConstructor
+    public enum UserRole {
+        USER("ROLE_USER"),
+        ADMIN("ROLE_ADMIN");
+
+        private final String value;
+    }
+}
