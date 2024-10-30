89ccd897ce80da37eefc47b4df17431820525072
junest66 <chlwndks333@gmail.com>
2024-10-30 14:58:02 +0900

    first commit

---
+++ src/main/java/com/groom/demo/HaenyeoApplication.java
@@ -0,0 +1,13 @@
+package com.groom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaenyeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaenyeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groom/demo/HaenyeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groom.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaenyeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
