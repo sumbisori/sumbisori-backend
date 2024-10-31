af4423fede02222fda2188f43f3ef1d6b7e04a16
junest66 <chlwndks333@gmail.com>
2024-11-01 02:30:50 +0900

    refactor: 누락된 한글이름 추가

--- src/main/java/com/groom/demo/domain/seafood/dto/MySeafoodDto.java
+++ src/main/java/com/groom/demo/domain/seafood/dto/MySeafoodDto.java
@@ -3,7 +3,7 @@ package com.groom.demo.domain.seafood.dto;
 import com.groom.demo.domain.seafood.Seafood;
 import java.time.LocalDateTime;
 
-public record MySeafoodDto(Seafood seafood, String name, String description,
+public record MySeafoodDto(Seafood seafood, String name, String englishName, String description,
                            LocalDateTime insDt, int count) {
 
 }
