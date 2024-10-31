65ee3b843fa4bc2ff1738468df97e93f25701aad
junest66 <chlwndks333@gmail.com>
2024-11-01 05:49:12 +0900

    refactor: 날짜 데이터 문자열로 변환

--- src/main/java/com/groom/demo/domain/seafood/dto/MySeafoodDto.java
+++ src/main/java/com/groom/demo/domain/seafood/dto/MySeafoodDto.java
@@ -2,8 +2,17 @@ package com.groom.demo.domain.seafood.dto;
 
 import com.groom.demo.domain.seafood.Seafood;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record MySeafoodDto(Seafood seafood, String name, String englishName, String description,
-                           LocalDateTime insDt, int count) {
+                           String insDt, int count) {
 
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy년 M월 d일");
+
+    public MySeafoodDto(Seafood seafood, String name, String englishName, String description,
+                        LocalDateTime insDt, int count) {
+        this(seafood, name, englishName, description,
+                insDt != null ? insDt.format(FORMATTER) : null, // null이면 변환하지 않음
+                count);
+    }
 }
