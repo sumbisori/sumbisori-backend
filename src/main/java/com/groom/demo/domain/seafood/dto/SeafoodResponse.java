809d7fa3164ff376dc3cfaf03376b37bcf060c66
junest66 <chlwndks333@gmail.com>
2024-11-05 00:35:50 +0900

    refactor: Seafood 종류 응답에 id 추가

--- src/main/java/com/groom/demo/domain/seafood/dto/SeafoodResponse.java
+++ src/main/java/com/groom/demo/domain/seafood/dto/SeafoodResponse.java
@@ -1,4 +1,9 @@
 package com.groom.demo.domain.seafood.dto;
 
-public record SeafoodResponse(String value, String name, String englishName, String description) {
+import com.groom.demo.domain.seafood.entity.Seafood;
+
+public record SeafoodResponse(Long seafoodId, String koreanName, String englishName, String description) {
+    public static SeafoodResponse from(Seafood seafood) {
+        return new SeafoodResponse(seafood.getId(), seafood.getKoreanName(), seafood.getEnglishName(), seafood.getDescription());
+    }
 }
