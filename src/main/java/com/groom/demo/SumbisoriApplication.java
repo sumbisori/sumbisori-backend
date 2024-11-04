3c6d94581726108d78251ee3065db38c55e0a6a7
junest66 <chlwndks333@gmail.com>
2024-11-04 22:41:39 +0900

    rename: 클래스명 변경

--- src/main/java/com/groom/demo/SumbisoriApplication.java
+++ src/main/java/com/groom/demo/SumbisoriApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HaenyeoApplication {
+public class SumbisoriApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HaenyeoApplication.class, args);
+		SpringApplication.run(SumbisoriApplication.class, args);
 	}
 
 }
