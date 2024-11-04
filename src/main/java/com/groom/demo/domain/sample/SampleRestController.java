c109e029f8d0fa54fcd5ba2bd50bd1ff26612bfe
junest66 <chlwndks333@gmail.com>
2024-11-05 00:36:46 +0900

    remove: 불필요한 api 제거

--- src/main/java/com/groom/demo/domain/sample/SampleRestController.java
+++ src/main/java/com/groom/demo/domain/sample/SampleRestController.java
@@ -1,32 +1,15 @@
 package com.groom.demo.domain.sample;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@CrossOrigin(origins = "*") // 모든 도메인에서 접근 허용
 public class SampleRestController {
-
-    private final SampleDataRepository sampleDataRepository;
-
     @GetMapping("/test")
     public ResponseEntity<?> pingTest() {
         return ResponseEntity.ok(true);
-    }
-
-    @GetMapping("/db")
-    public ResponseEntity<?> dbTest() {
-        List<SampleData> sampleDataList = sampleDataRepository.findAll();
-        return ResponseEntity.ok(sampleDataList);
-    }
-
-    @GetMapping("/connect")
-    public ResponseEntity<?> strTest() {
-        return ResponseEntity.ok("해남해녀입니다.");
     }
 }
