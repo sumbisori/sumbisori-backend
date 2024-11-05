a7514cac496b3c6f0128ab6e59ef2b9491166ed6
junest66 <chlwndks333@gmail.com>
2024-11-05 18:14:15 +0900

    fix: 프록시 설정 변경

--- src/main/java/com/groom/demo/common/config/ProxyConfig.java
+++ src/main/java/com/groom/demo/common/config/ProxyConfig.java
@@ -1,23 +1,32 @@
 package com.groom.demo.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@Profile("prod")
 public class ProxyConfig {
+
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
 
     @Bean
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("krmp-proxy.9rum.cc", 3128));
-        requestFactory.setProxy(proxy);
+
+        // spring.profiles.active가 'prod'일 때만 프록시 설정을 적용
+        if ("prod".equals(activeProfile)) {
+            String proxyHost = "krmp-proxy.9rum.cc";
+            int proxyPort = 3128;
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+            requestFactory.setProxy(proxy);
+        }
+
         return new RestTemplate(requestFactory);
     }
 }
