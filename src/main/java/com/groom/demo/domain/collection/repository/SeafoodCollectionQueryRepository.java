edbaa192ca1d7211a8591c3930570dcc4187ae4a
junest66 <chlwndks333@gmail.com>
2024-11-05 09:34:10 +0900

    feat: 수집된 해산물 조회 쿼리 추가

---
+++ src/main/java/com/groom/demo/domain/collection/repository/SeafoodCollectionQueryRepository.java
@@ -0,0 +1,31 @@
+package com.groom.demo.domain.collection.repository;
+
+import static com.groom.demo.domain.collection.entity.QSeafoodCollection.seafoodCollection;
+import static com.groom.demo.domain.seafood.entity.QSeafood.seafood;
+
+import com.groom.demo.domain.collection.dto.response.MyCollectionSeafood;
+import com.groom.demo.domain.collection.dto.response.QMyCollectionSeafood;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class SeafoodCollectionQueryRepository {
+    private final JPAQueryFactory queryFactory;
+
+    public List<MyCollectionSeafood> findSeafoodCollectionByUserId(Long userId) {
+        return queryFactory
+                .select(new QMyCollectionSeafood(
+                        seafood.id,
+                        seafood.koreanName,
+                        seafood.englishName,
+                        seafoodCollection.count().intValue()
+                ))
+                .from(seafoodCollection)
+                .join(seafoodCollection.seafood, seafood).on(seafoodCollection.userId.eq(userId))
+                .groupBy(seafood.id, seafood.koreanName, seafood.englishName)
+                .fetch();
+    }
+}
