166787ee8d477dcda1aa0a799f5215559e16c1b1
junest66 <chlwndks333@gmail.com>
2024-11-05 09:35:25 +0900

    refactor: 나의 수집 현황 응답 DTO 리팩토링

--- src/main/java/com/groom/demo/domain/seafood/dto/MySeafoodDto.java
+++ src/main/java/com/groom/demo/domain/seafood/dto/MySeafoodDto.java
@@ -1,18 +1,11 @@
 package com.groom.demo.domain.seafood.dto;
 
-import com.groom.demo.domain.seafood.Seafood;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import com.querydsl.core.annotations.QueryProjection;
 
-public record MySeafoodDto(Seafood seafood, String name, String englishName, String description,
+public record MySeafoodDto(Long seafoodId, String koreanName, String englishName, String description,
                            String insDt, int count) {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy년 M월 d일");
-
-    public MySeafoodDto(Seafood seafood, String name, String englishName, String description,
-                        LocalDateTime insDt, int count) {
-        this(seafood, name, englishName, description,
-                insDt != null ? insDt.format(FORMATTER) : null, // null이면 변환하지 않음
-                count);
+    @QueryProjection
+    public MySeafoodDto {
     }
 }
