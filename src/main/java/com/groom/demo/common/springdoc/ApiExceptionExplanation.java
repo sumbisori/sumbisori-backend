3c1fab4d5ea317f84f194c044c86e7d9d987efe7
junest66 <chlwndks333@gmail.com>
2024-11-05 14:48:40 +0900

    feat: spring doc custom error 어노테이션 추가

---
+++ src/main/java/com/groom/demo/common/springdoc/ApiExceptionExplanation.java
@@ -0,0 +1,22 @@
+package com.groom.demo.common.springdoc;
+
+import com.groom.demo.common.error.ErrorCode;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApiExceptionExplanation {
+    Class<? extends ErrorCode> value();
+
+    /**
+     * BaseErrorCode를 구현한 Enum 클래스의 상수명
+     */
+    String constant();
+
+    String name() default "";
+
+    String description() default "";
+}
