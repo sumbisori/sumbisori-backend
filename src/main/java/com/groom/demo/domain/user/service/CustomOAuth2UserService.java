0cd96e4308733b6e40a393c06538efcd452523ed
junest66 <chlwndks333@gmail.com>
2024-11-05 17:55:40 +0900

    refactor: 프로식 롤백

--- src/main/java/com/groom/demo/domain/user/service/CustomOAuth2UserService.java
+++ src/main/java/com/groom/demo/domain/user/service/CustomOAuth2UserService.java
@@ -5,17 +5,14 @@ import com.groom.demo.domain.user.dto.KaKaoResponse;
 import com.groom.demo.domain.user.dto.OAuth2Response;
 import com.groom.demo.domain.user.entity.User;
 import com.groom.demo.domain.user.repository.UserRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
@@ -24,14 +21,6 @@ import org.springframework.web.client.RestTemplate;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
-
-    @Autowired
-    private RestTemplate proxiedRestTemplate;
-
-    @PostConstruct
-    public void init() {
-        super.setRestOperations(proxiedRestTemplate);
-    }
 
     @Transactional
     @Override
