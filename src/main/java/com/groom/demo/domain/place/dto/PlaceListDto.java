cd4612cbfe844ebad62a53cc6159fd9482be035c
junest66 <chlwndks333@gmail.com>
2024-11-05 12:28:23 +0900

    feat: Place 응답 DTO 추가

--- src/main/java/com/groom/demo/domain/place/dto/PlaceListDto.java
+++
@@ -1,8 +0,0 @@
-package com.groom.demo.domain.place.dto;
-
-import java.util.List;
-
-public record PlaceListDto(String value, String name, String address,
-                           int price, String desc, String imageUrl, List<String> availableDate,
-                           int x, int y) {
-}
