1bfe09e29b90e16084d7a63ae3ebb5b9e52710e3
junest66 <chlwndks333@gmail.com>
2024-11-05 13:51:51 +0900

    refactor: 나의 수집 갯수 쿼리 적용

--- src/main/java/com/groom/demo/domain/user/service/UserService.java
+++ src/main/java/com/groom/demo/domain/user/service/UserService.java
@@ -1,9 +1,13 @@
 package com.groom.demo.domain.user.service;
 
-import com.groom.demo.domain.book.BookRepository;
-import com.groom.demo.domain.user.SignRequest;
-import com.groom.demo.domain.user.UserProfile;
+import com.groom.demo.domain.collection.repository.SeafoodCollectionQueryRepository;
+import com.groom.demo.domain.collection.repository.SeafoodCollectionRepository;
+import com.groom.demo.domain.user.dto.UserProfile;
+import com.groom.demo.domain.user.dto.request.LoginRequest;
+import com.groom.demo.domain.user.dto.request.SignRequest;
+import com.groom.demo.domain.user.dto.response.LoginResponse;
 import com.groom.demo.domain.user.entity.User;
+import com.groom.demo.domain.user.entity.User.UserRole;
 import com.groom.demo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +16,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final BookRepository bookRepository;
+    private final SeafoodCollectionQueryRepository seafoodCollectionQueryRepository;
 
     public UserProfile getMyInfo(Long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
-        int count = bookRepository.countByUserId(userId);
+        int count = seafoodCollectionQueryRepository.sumQuantityByUserId(userId);
         return UserProfile.builder()
                 .count(count)
                 .nickname(user.getNickname())
@@ -24,23 +28,23 @@ public class UserService {
     }
 
     public void signup(SignRequest signRequest) {
-        //중복검사
         if (userRepository.findByNickname(signRequest.getNickname()).isPresent()) {
             throw new IllegalArgumentException("이미 사용중인 닉네임입니다.");
         }
         userRepository.save(User.builder()
                 .nickname(signRequest.getNickname())
                 .password(signRequest.getPassword())
+                .userRole(UserRole.USER)
                 .build());
     }
 
-    public Long login(SignRequest signRequest) {
-        User user = userRepository.findByNickname(signRequest.getNickname())
+    public LoginResponse login(LoginRequest loginRequest) {
+        User user = userRepository.findByNickname(loginRequest.getNickname())
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
-        if (!user.getPassword().equals(signRequest.getPassword())) {
+        if (!user.getPassword().equals(loginRequest.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
-        return user.getId();
+        return new LoginResponse(user.getId());
     }
 }
