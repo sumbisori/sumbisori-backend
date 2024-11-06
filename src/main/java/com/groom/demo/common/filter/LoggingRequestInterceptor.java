cd3e67a3a4b703e91ba04eedc37f44e002604a04
junest66 <chlwndks333@gmail.com>
2024-11-06 16:03:50 +0900

    chore: 로깅 설정

--- src/main/java/com/groom/demo/common/filter/LoggingRequestInterceptor.java
+++ src/main/java/com/groom/demo/common/filter/LoggingRequestInterceptor.java
@@ -1,15 +1,13 @@
 package com.groom.demo.common.filter;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.util.StreamUtils;
 
 @Slf4j
 public class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
@@ -19,7 +17,10 @@ public class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
             HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
         logRequest(request, body);
         ClientHttpResponse response = execution.execute(request, body);
-        return logResponse(response);
+        // 응답을 래핑하여 여러 번 읽을 수 있도록 합니다.
+        ClientHttpResponse responseCopy = new BufferingClientHttpResponseWrapper(response);
+        logResponse(responseCopy);
+        return responseCopy;
     }
 
     private void logRequest(HttpRequest request, byte[] body) throws IOException {
@@ -31,21 +32,13 @@ public class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
         log.debug("===== Request End =====");
     }
 
-    private ClientHttpResponse logResponse(ClientHttpResponse response) throws IOException {
-        ClientHttpResponse responseCopy = new BufferingClientHttpResponseWrapper(response);
-
-        String responseBody = new BufferedReader(
-                new InputStreamReader(responseCopy.getBody(), StandardCharsets.UTF_8))
-                .lines()
-                .collect(Collectors.joining("\n"));
-
+    private void logResponse(ClientHttpResponse response) throws IOException {
+        String responseBody = StreamUtils.copyToString(response.getBody(), StandardCharsets.UTF_8);
         log.debug("===== Response Begin =====");
-        log.debug("Status code  : {}", responseCopy.getStatusCode());
-        log.debug("Status text  : {}", responseCopy.getStatusText());
-        log.debug("Headers      : {}", responseCopy.getHeaders());
+        log.debug("Status code  : {}", response.getStatusCode());
+        log.debug("Status text  : {}", response.getStatusText());
+        log.debug("Headers      : {}", response.getHeaders());
         log.debug("Response body: {}", responseBody);
         log.debug("===== Response End =====");
-
-        return responseCopy;
     }
 }
