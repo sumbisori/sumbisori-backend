c4b28cb5d41a68d1abc2c333e8815f416eeaf46b
junest66 <chlwndks333@gmail.com>
2024-11-06 22:38:14 +0900

    refactor: 로그인 어노테이션 추가

--- src/main/java/com/groom/demo/domain/reservation/controller/ReservationApi.java
+++ src/main/java/com/groom/demo/domain/reservation/controller/ReservationApi.java
@@ -1,5 +1,6 @@
 package com.groom.demo.domain.reservation.controller;
 
+import com.groom.demo.common.config.LoginUser;
 import com.groom.demo.domain.reservation.dto.ReservationCount;
 import com.groom.demo.domain.reservation.dto.ReservationDto;
 import com.groom.demo.domain.reservation.dto.ReservationRequest;
@@ -9,20 +10,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "reservations", description = "예약 API")
 public interface ReservationApi {
 
     @Operation(summary = "예약 목록 조회 - 인증")
-    public ResponseEntity<List<ReservationDto>> getMyReservations(@RequestHeader("userId") Long userId,
+    public ResponseEntity<List<ReservationDto>> getMyReservations(@LoginUser Long userId,
                                                                   @RequestParam Status status);
 
     @Operation(summary = "예약 목록 갯수 조회 - 인증")
-    public ResponseEntity<ReservationCount> getMyReservationCount(@RequestHeader("userId") Long userId);
+    public ResponseEntity<ReservationCount> getMyReservationCount(@LoginUser Long userId);
 
     @Operation(summary = "예약 생성")
-    public ResponseEntity<Void> createReservation(@RequestHeader("userId") Long userId,
-                                               @RequestBody ReservationRequest reservationRequest);
+    public ResponseEntity<Void> createReservation(@LoginUser Long userId,
+                                                  @RequestBody ReservationRequest reservationRequest);
 }
