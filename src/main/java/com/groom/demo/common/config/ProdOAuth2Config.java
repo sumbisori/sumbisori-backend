d893a08d3e50ff4e2542f73f4dea05f5c40dda53
junest66 <chlwndks333@gmail.com>
2024-11-06 17:50:44 +0900

    fix: 프록시를 prod 버그수정

--- src/main/java/com/groom/demo/common/config/ProdOAuth2Config.java
+++ src/main/java/com/groom/demo/common/config/ProdOAuth2Config.java
@@ -1,25 +1,32 @@
 package com.groom.demo.common.config;
 
 import com.groom.demo.common.filter.LoggingRequestInterceptor;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpHost;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-
 @Configuration
-@Profile("prod")  // prod 프로필이 활성화된 경우에만 이 설정 적용
+@Profile("prod")
 public class ProdOAuth2Config {
 
     @Bean
     public RestTemplate oAuthRestTemplate() {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        // 프록시 설정
+        HttpHost proxy = new HttpHost("krmp-proxy.9rum.cc", 3128);
 
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("krmp-proxy.9rum.cc", 3128));
-        requestFactory.setProxy(proxy);
+        // 프록시 설정이 적용된 HttpClient 생성
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setProxy(proxy)
+                .build();
+
+        // 커스텀 HttpClient를 사용하는 RequestFactory 생성
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
 
         RestTemplate restTemplate = new RestTemplate(requestFactory);
         restTemplate.getInterceptors().add(new LoggingRequestInterceptor());
