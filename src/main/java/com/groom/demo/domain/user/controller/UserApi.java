65efca6596193513e43c9bf277ef89dc6752cc8d
junest66 <chlwndks333@gmail.com>
2024-11-06 22:36:40 +0900

    refactor: 로그인/회원가입 api 비활성화 및 로그아웃 추가

--- src/main/java/com/groom/demo/domain/user/controller/UserApi.java
+++ src/main/java/com/groom/demo/domain/user/controller/UserApi.java
@@ -1,41 +1,34 @@
 package com.groom.demo.domain.user.controller;
 
-import static com.groom.demo.domain.user.error.UserErrorCode.Const.NICKNAME_DUPLICATION;
-import static com.groom.demo.domain.user.error.UserErrorCode.Const.USER_NOT_FOUND;
-import static com.groom.demo.domain.user.error.UserErrorCode.Const.USER_NOT_MATCHED;
-
-import com.groom.demo.common.springdoc.ApiExceptionExplanation;
-import com.groom.demo.common.springdoc.ApiResponseExplanations;
-import com.groom.demo.domain.user.dto.request.LoginRequest;
-import com.groom.demo.domain.user.dto.request.SignRequest;
 import com.groom.demo.domain.user.dto.UserProfile;
-import com.groom.demo.domain.user.dto.response.LoginResponse;
-import com.groom.demo.domain.user.error.UserErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "users", description = "유저 API")
 public interface UserApi {
     @Operation(summary = "프로필 조회 - 인증")
-    public ResponseEntity<UserProfile> getMyInfo(@RequestHeader("userId") Long userId);
+    public ResponseEntity<UserProfile> getMyInfo(Long userId);
 
-    @Operation(summary = "회원가입")
-    @ApiResponseExplanations(
-            errors = {
-                    @ApiExceptionExplanation(value = UserErrorCode.class, constant = NICKNAME_DUPLICATION, name = "닉네임이 중복됐을때"),
-            }
-    )
-    public ResponseEntity<Void> signup(@RequestBody SignRequest signRequest);
+    @Operation(summary = "로그아웃")
+    public ResponseEntity<Void> logout(HttpServletResponse response);
 
-    @Operation(summary = "로그인")
-    @ApiResponseExplanations(
-            errors = {
-                    @ApiExceptionExplanation(value = UserErrorCode.class, constant = USER_NOT_FOUND, name = "존재하지 않는 닉네임일때"),
-                    @ApiExceptionExplanation(value = UserErrorCode.class, constant = USER_NOT_MATCHED, name = "비밀번호가 일치하지 않을 때"),
-            }
-    )
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest);
+//    @Operation(summary = "회원가입")
+//    @ApiResponseExplanations(
+//            errors = {
+//                    @ApiExceptionExplanation(value = UserErrorCode.class, constant = NICKNAME_DUPLICATION, name = "닉네임이 중복됐을때"),
+//            }
+//    )
+//    public ResponseEntity<Void> signup(@RequestBody SignRequest signRequest);
+//
+//    @Operation(summary = "로그인")
+//    @ApiResponseExplanations(
+//            errors = {
+//                    @ApiExceptionExplanation(value = UserErrorCode.class, constant = USER_NOT_FOUND, name = "존재하지 않는 닉네임일때"),
+//                    @ApiExceptionExplanation(value = UserErrorCode.class, constant = USER_NOT_MATCHED, name = "비밀번호가 일치하지 않을 때"),
+//            }
+//    )
+//    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest);
 }
--- src/main/java/com/groom/demo/domain/user/controller/UserController.java
+++ src/main/java/com/groom/demo/domain/user/controller/UserController.java
@@ -1,17 +1,15 @@
 package com.groom.demo.domain.user.controller;
 
+import com.groom.demo.common.config.LoginUser;
+import com.groom.demo.common.util.CookieUtil;
+import com.groom.demo.domain.token.entity.TokenType;
 import com.groom.demo.domain.user.dto.UserProfile;
-import com.groom.demo.domain.user.dto.request.LoginRequest;
-import com.groom.demo.domain.user.dto.request.SignRequest;
-import com.groom.demo.domain.user.dto.response.LoginResponse;
 import com.groom.demo.domain.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,19 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController implements UserApi {
     private final UserService userService;
 
+    @Override
     @GetMapping
-    public ResponseEntity<UserProfile> getMyInfo(@RequestHeader("userId") Long userId) {
+    public ResponseEntity<UserProfile> getMyInfo(@LoginUser Long userId) {
         return ResponseEntity.ok(userService.getMyInfo(userId));
     }
 
-    @PostMapping
-    public ResponseEntity<Void> signup(@RequestBody SignRequest signRequest) {
-        userService.signup(signRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    @Override
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletResponse response) {
+        CookieUtil.expireCookie(response, TokenType.ACCESS.name());
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.login(loginRequest));
-    }
+    //    @PostMapping
+//    public ResponseEntity<Void> signup(@RequestBody SignRequest signRequest) {
+//        userService.signup(signRequest);
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+//        return ResponseEntity.status(HttpStatus.CREATED).body(userService.login(loginRequest));
+//    }
 }
