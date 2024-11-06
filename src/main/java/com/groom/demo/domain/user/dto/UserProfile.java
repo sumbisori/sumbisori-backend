c9a965e47882ff2f666d5145687884e01d4a5f31
junest66 <chlwndks333@gmail.com>
2024-11-06 22:37:06 +0900

    refactor: 로그인/회원가입 서비스 함수 주석 및 프로필 조회에 이미지 추가

--- src/main/java/com/groom/demo/domain/user/dto/UserProfile.java
+++ src/main/java/com/groom/demo/domain/user/dto/UserProfile.java
@@ -3,5 +3,5 @@ package com.groom.demo.domain.user.dto;
 import lombok.Builder;
 
 @Builder
-public record UserProfile(String nickname, int count) {
+public record UserProfile(String nickname, String profileImageUrl, int count) {
 }
