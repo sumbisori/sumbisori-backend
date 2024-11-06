bf509e77d40432620ddf7210a7be25d0a1d71530
junest66 <chlwndks333@gmail.com>
2024-11-06 22:35:24 +0900

    feat: 커스텀 인증 및 인가 에러 핸들러 추가

---
+++ src/main/java/com/groom/demo/common/error/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.groom.demo.common.error.handler;
+
+import com.groom.demo.common.error.GlobalErrorCode;
+import com.groom.demo.common.error.GlobalException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        throw new GlobalException(GlobalErrorCode.ACCESS_DENIED);
+    }
+}
+
