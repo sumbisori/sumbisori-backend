e82f1eb79f829e825a5290cd1cc3779d74631ffa
junest66 <chlwndks333@gmail.com>
2024-11-06 22:37:01 +0900

    refactor: 로그인/회원가입 서비스 함수 주석 및 프로필 조회에 이미지 추가

--- src/main/java/com/groom/demo/domain/user/service/UserService.java
+++ src/main/java/com/groom/demo/domain/user/service/UserService.java
@@ -26,28 +26,29 @@ public class UserService {
         int count = seafoodCollectionQueryRepository.sumQuantityByUserId(userId);
         return UserProfile.builder()
                 .count(count)
+                .profileImageUrl(user.getProfileImageUrl())
                 .nickname(user.getNickname())
                 .build();
     }
 
-    public void signup(SignRequest signRequest) {
-        if (userRepository.findByNickname(signRequest.getNickname()).isPresent()) {
-            throw new UserException(UserErrorCode.NICKNAME_DUPLICATION);
-        }
-        userRepository.save(User.builder()
-                .nickname(signRequest.getNickname())
-                .password(signRequest.getPassword())
-                .userRole(UserRole.USER)
-                .build());
-    }
-
-    public LoginResponse login(LoginRequest loginRequest) {
-        User user = userRepository.findByNickname(loginRequest.getNickname())
-                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
-
-        if (!user.getPassword().equals(loginRequest.getPassword())) {
-            throw new UserException(UserErrorCode.USER_NOT_MATCHED);
-        }
-        return new LoginResponse(user.getId());
-    }
+//    public void signup(SignRequest signRequest) {
+//        if (userRepository.findByNickname(signRequest.getNickname()).isPresent()) {
+//            throw new UserException(UserErrorCode.NICKNAME_DUPLICATION);
+//        }
+//        userRepository.save(User.builder()
+//                .nickname(signRequest.getNickname())
+//                .password(signRequest.getPassword())
+//                .userRole(UserRole.USER)
+//                .build());
+//    }
+//
+//    public LoginResponse login(LoginRequest loginRequest) {
+//        User user = userRepository.findByNickname(loginRequest.getNickname())
+//                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+//
+//        if (!user.getPassword().equals(loginRequest.getPassword())) {
+//            throw new UserException(UserErrorCode.USER_NOT_MATCHED);
+//        }
+//        return new LoginResponse(user.getId());
+//    }
 }
