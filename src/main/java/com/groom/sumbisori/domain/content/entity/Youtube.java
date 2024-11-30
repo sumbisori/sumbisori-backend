44268dc42f964c16eaa42a4721b0eaa455b09121
junest66 <chlwndks333@gmail.com>
2024-12-01 01:29:31 +0900

    feat: Youtube Entity 추가

---
+++ src/main/java/com/groom/sumbisori/domain/content/entity/Youtube.java
@@ -0,0 +1,37 @@
+package com.groom.sumbisori.domain.content.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class Youtube {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "youtube_id")
+    private Long id;
+
+    @Column
+    private String videoId;
+
+    @Column
+    private String title;
+
+    @Column
+    private String thumbnailUrl;
+
+    @Column
+    private LocalDateTime publishTime;
+}
