63a243b8b4a3522943599b22e00d81965f4689f9
junest66 <chlwndks333@gmail.com>
2024-12-12 01:27:21 +0900

    refactor: 유튜브 스케줄러 프로필 지정

--- src/main/java/com/groom/sumbisori/domain/content/service/YoutubeService.java
+++ src/main/java/com/groom/sumbisori/domain/content/service/YoutubeService.java
@@ -17,10 +17,12 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("prod")
 @RequiredArgsConstructor
 @Slf4j
 public class YoutubeService {
