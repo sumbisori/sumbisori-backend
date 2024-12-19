1c16bf39c9c27716cf0f16f2671672e5a2312b7c
junest66 <chlwndks333@gmail.com>
2024-12-20 00:46:46 +0900

    refactor: 전화번호 응답 필드 변환 로직 추가
    #23

--- src/main/java/com/groom/sumbisori/domain/place/dto/PlaceResponse.java
+++ src/main/java/com/groom/sumbisori/domain/place/dto/PlaceResponse.java
@@ -1,10 +1,15 @@
 package com.groom.sumbisori.domain.place.dto;
 
 import com.groom.sumbisori.domain.place.entity.Place;
+import java.util.Arrays;
+import java.util.List;
 
 public record PlaceResponse(Long placeId, String name, String address,
-                            int price, String desc, String imageUrl, double latitude, double longitude, String phoneNumber,
+                            int price, String desc, String imageUrl, double latitude, double longitude, List<String> phoneNumber,
                             String link) {
+
+    private static final String PHONE_NUMBER_DELIMITER = ",";
+
     public static PlaceResponse from(Place place) {
         return new PlaceResponse(
                 place.getId(),
@@ -15,8 +20,12 @@ public record PlaceResponse(Long placeId, String name, String address,
                 place.getImageUrl(),
                 place.getLatitude(),
                 place.getLongitude(),
-                place.getPhoneNumber(),
+                splitPhoneNumber(place.getPhoneNumber()),
                 place.getLink()
         );
+    }
+
+    private static List<String> splitPhoneNumber(String phoneNumber) {
+        return Arrays.stream(phoneNumber.split(PHONE_NUMBER_DELIMITER)).toList();
     }
 }
