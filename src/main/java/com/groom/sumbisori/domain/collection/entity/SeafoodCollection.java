f786a5621de6af96245bda3588cedaee9173be3c
junest66 <chlwndks333@gmail.com>
2024-12-20 17:55:42 +0900

    refactor: 기본 생성자 접근 제어자 수정

--- src/main/java/com/groom/sumbisori/domain/collection/entity/SeafoodCollection.java
+++ src/main/java/com/groom/sumbisori/domain/collection/entity/SeafoodCollection.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class SeafoodCollection extends BaseTimeEntity {
     @Id
