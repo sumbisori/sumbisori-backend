c4ab3241c5f2fac142f8ecc53d87230c6b63096e
junest66 <chlwndks333@gmail.com>
2024-12-21 01:06:28 +0900

    feat: 회원가입 이벤트 static 객체 생성 메서드 추가
    #26

--- src/main/java/com/groom/sumbisori/domain/badge/dto/event/SignUpEvent.java
+++ src/main/java/com/groom/sumbisori/domain/badge/dto/event/SignUpEvent.java
@@ -1,4 +1,7 @@
 package com.groom.sumbisori.domain.badge.dto.event;
 
 public record SignUpEvent(Long userId) {
+    static public SignUpEvent of(Long userId) {
+        return new SignUpEvent(userId);
+    }
 }
