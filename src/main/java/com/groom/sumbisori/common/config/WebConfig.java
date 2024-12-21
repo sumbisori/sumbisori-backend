742e7771ddd243c6e0cedbbe8d14814a79a3c62b
junest66 <chlwndks333@gmail.com>
2024-12-21 22:38:25 +0900

    refactor: API prefix 설정 변경

--- src/main/java/com/groom/sumbisori/common/config/WebConfig.java
+++ src/main/java/com/groom/sumbisori/common/config/WebConfig.java
@@ -3,14 +3,25 @@ package com.groom.sumbisori.common.config;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private static final String API_PREFIX = "/api";
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix(API_PREFIX,
+                HandlerTypePredicate.forAnnotation(RestController.class, Controller.class));
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
