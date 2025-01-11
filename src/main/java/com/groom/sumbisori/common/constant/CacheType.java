6c72ba0d1ed3c1e27a7ecce49585222a1f325301
junest66 <chlwndks333@gmail.com>
2025-01-12 05:41:49 +0900

    feat: 파도 정보 캐시 추가
    #34

--- src/main/java/com/groom/sumbisori/common/constant/CacheType.java
+++ src/main/java/com/groom/sumbisori/common/constant/CacheType.java
@@ -6,9 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CacheType {
-    PLACES("places", 60 * 60 * 24 * 30, 20),
-    PLACE_DETAILS("placeDetails", 60 * 60 * 24 * 30, 20),
-    YOUTUBES("youtubes", 60 * 60 * 24, 60);
+    PLACES("places", 60 * 60 * 24 * 30, 20),          // 30일
+    PLACE_DETAILS("placeDetails", 60 * 60 * 24 * 30, 20), // 30일
+    YOUTUBES("youtubes", 60 * 60 * 24, 60),           // 24시간
+    WAVES("waves", 60 * 62, 20);                      // 62분
 
     private final String cacheName;
     private final int expireSeconds;
