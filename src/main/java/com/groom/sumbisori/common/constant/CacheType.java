4c2e80aed018aea66606479f1d148dba665202be
junest66 <chlwndks333@gmail.com>
2025-01-13 05:13:23 +0900

    feat: 날씨 캐시 타입 추가
    #37

--- src/main/java/com/groom/sumbisori/common/constant/CacheType.java
+++ src/main/java/com/groom/sumbisori/common/constant/CacheType.java
@@ -9,7 +9,8 @@ public enum CacheType {
     PLACES("places", 60 * 60 * 24 * 30, 20),          // 30일
     PLACE_DETAILS("placeDetails", 60 * 60 * 24 * 30, 20), // 30일
     YOUTUBES("youtubes", 60 * 60 * 24, 60),           // 24시간
-    WAVES("waves", 60 * 62, 20);                      // 62분
+    WAVES("waves", 60 * 62, 20),                      // 1시간 2분
+    WEATHERS("weathers", 60 * 90, 20);                // 1시간 30분
 
     private final String cacheName;
     private final int expireSeconds;
