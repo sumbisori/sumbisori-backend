1ac741d2448a0f53f0d6a8ae744bde795b2afe6b
junest66 <chlwndks333@gmail.com>
2025-01-25 18:29:21 +0900

    refactor: 해녀체험 장소 설명 필드 json 문자열로 변경
    #43

--- src/main/java/com/groom/sumbisori/domain/place/dto/PlaceResponse.java
+++ src/main/java/com/groom/sumbisori/domain/place/dto/PlaceResponse.java
@@ -1,31 +1,25 @@
 package com.groom.sumbisori.domain.place.dto;
 
+import com.groom.sumbisori.common.util.JsonUtil;
 import com.groom.sumbisori.domain.place.entity.Place;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
 
 public record PlaceResponse(Long placeId, String name, String address,
-                            int price, String desc, String imageUrl, double latitude, double longitude, List<String> phoneNumber,
+                            int price, Map<String, String> desc, String imageUrl, double latitude, double longitude,
+                            String phoneNumber,
                             String link) {
-
-    private static final String PHONE_NUMBER_DELIMITER = ",";
-
     public static PlaceResponse from(Place place) {
         return new PlaceResponse(
                 place.getId(),
                 place.getName(),
                 place.getAddress(),
                 place.getPrice(),
-                place.getDescription(),
+                JsonUtil.parseJsonToMap(place.getDescription()),
                 place.getImageUrl(),
                 place.getLatitude(),
                 place.getLongitude(),
-                splitPhoneNumber(place.getPhoneNumber()),
+                place.getPhoneNumber(),
                 place.getLink()
         );
-    }
-
-    private static List<String> splitPhoneNumber(String phoneNumber) {
-        return Arrays.stream(phoneNumber.split(PHONE_NUMBER_DELIMITER)).toList();
     }
 }
