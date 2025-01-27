d1974fb0ef8544f4fef37ac6cbbe226750b9be3f
junest66 <chlwndks333@gmail.com>
2025-01-27 18:21:17 +0900

    refactor: n+1 문제 해결을 위한 fetchJoin 설정
    #45

---
+++ src/main/java/com/groom/sumbisori/domain/place/repository/PlaceQueryRepository.java
@@ -0,0 +1,32 @@
+package com.groom.sumbisori.domain.place.repository;
+
+import static com.groom.sumbisori.domain.place.entity.QPlace.place;
+import static com.groom.sumbisori.domain.place.entity.QPlaceDescription.placeDescription;
+
+import com.groom.sumbisori.domain.place.entity.Place;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PlaceQueryRepository {
+    private final JPAQueryFactory queryFactory;
+
+    public Optional<Place> findById(Long placeId) {
+        Place result = queryFactory.selectFrom(place)
+                .leftJoin(place.descriptions, placeDescription).fetchJoin()
+                .where(place.id.eq(placeId))
+                .fetchOne();
+        return Optional.ofNullable(result);
+    }
+
+    public List<Place> findAll() {
+        return queryFactory.selectFrom(place)
+                .leftJoin(place.descriptions, placeDescription).fetchJoin()
+                .distinct()
+                .fetch();
+    }
+}
--- src/main/java/com/groom/sumbisori/domain/place/service/PlaceService.java
+++ src/main/java/com/groom/sumbisori/domain/place/service/PlaceService.java
@@ -5,6 +5,7 @@ import com.groom.sumbisori.domain.place.dto.PlaceResponse;
 import com.groom.sumbisori.domain.place.entity.Place;
 import com.groom.sumbisori.domain.place.error.PlaceErrorcode;
 import com.groom.sumbisori.domain.place.error.exception.PlaceException;
+import com.groom.sumbisori.domain.place.repository.PlaceQueryRepository;
 import com.groom.sumbisori.domain.place.repository.PlaceRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -17,17 +18,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PlaceService {
     private final PlaceRepository placeRepository;
+    private final PlaceQueryRepository placeQueryRepository;
 
     @Cacheable(cacheNames = "places")
     public List<PlaceResponse> getAllPlaces() {
-        return placeRepository.findAll().stream()
+        return placeQueryRepository.findAll().stream()
                 .map(place -> PlaceResponse.from(place))
                 .toList();
     }
 
     @Cacheable(cacheNames = "placeDetails", key = "#placeId")
     public PlaceResponse getPlaceById(Long placeId) {
-        Place place = placeRepository.findById(placeId)
+        Place place = placeQueryRepository.findById(placeId)
                 .orElseThrow(() -> new PlaceException(PlaceErrorcode.PLACE_NOT_FOUND));
         return PlaceResponse.from(place);
     }
