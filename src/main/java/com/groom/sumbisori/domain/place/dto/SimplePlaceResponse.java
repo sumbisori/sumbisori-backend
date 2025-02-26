00276e650d9a9d27209441b87209194fa11fa489
junest66 <chlwndks333@gmail.com>
2025-02-26 23:54:09 +0900

    refactor: 주소에서 시를 추출해 응답필드 추가
    #60

--- src/main/java/com/groom/sumbisori/domain/place/dto/SimplePlaceResponse.java
+++ src/main/java/com/groom/sumbisori/domain/place/dto/SimplePlaceResponse.java
@@ -2,12 +2,20 @@ package com.groom.sumbisori.domain.place.dto;
 
 import com.groom.sumbisori.domain.place.entity.Place;
 
-public record SimplePlaceResponse(Long placeId, String name, String imageUrl) {
+public record SimplePlaceResponse(Long placeId, String name, String city, String imageUrl) {
+    private static final int CITY_INDEX = 1;
+
     public static SimplePlaceResponse from(Place place) {
         return new SimplePlaceResponse(
                 place.getId(),
                 place.getName(),
+                getCityFromAddress(place.getAddress()),
                 place.getImageUrl()
         );
+    }
+
+    private static String getCityFromAddress(String address) {
+        String[] addressParts = address.split(" ");
+        return addressParts.length > CITY_INDEX ? addressParts[CITY_INDEX] : "";
     }
 }
