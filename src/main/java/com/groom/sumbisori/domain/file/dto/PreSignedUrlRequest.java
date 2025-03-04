e3deeee23adab5895df2da2841001ce499d4082e
junest66 <chlwndks333@gmail.com>
2025-03-05 00:18:12 +0900

    feat: preSigned-url 발급 요청 DTO 추가
    #63

---
+++ src/main/java/com/groom/sumbisori/domain/file/dto/PreSignedUrlRequest.java
@@ -0,0 +1,12 @@
+package com.groom.sumbisori.domain.file.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+
+public record PreSignedUrlRequest(
+        @NotNull(message = "파일 정보는 필수입니다.")
+        @Size(min = 1, max = 10, message = "최소 1개, 최대 10개의 파일을 요청할 수 있습니다.")
+        List<FileInfo> fileInfos
+) {
+}
