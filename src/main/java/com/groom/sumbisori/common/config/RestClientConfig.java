836f74cc9baabfe58061e802f2eb30f8df8d6c4b
junest66 <chlwndks333@gmail.com>
2025-03-06 21:48:23 +0900

    chore: 외부 API 호출에 타임아웃 설정
    #69

--- src/main/java/com/groom/sumbisori/common/config/RestClientConfig.java
+++ src/main/java/com/groom/sumbisori/common/config/RestClientConfig.java
@@ -1,13 +1,29 @@
 package com.groom.sumbisori.common.config;
 
+import java.time.Duration;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+    private static final int CONNECT_TIMEOUT_SECONDS = 5;
+    private static final int READ_TIMEOUT_SECONDS = 5;
+
     @Bean
     RestClient restClient() {
-        return RestClient.create();
+        return RestClient.builder()
+                .requestFactory(customRequestFactory())
+                .build();
+    }
+
+    private ClientHttpRequestFactory customRequestFactory() {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(CONNECT_TIMEOUT_SECONDS))
+                .withReadTimeout(Duration.ofSeconds(READ_TIMEOUT_SECONDS));
+        return ClientHttpRequestFactories.get(settings);
     }
 }
