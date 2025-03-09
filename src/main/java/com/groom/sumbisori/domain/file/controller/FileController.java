02b577685bb1921848b52dbbe71f7a57a4417d04
junest66 <chlwndks333@gmail.com>
2025-03-09 21:05:03 +0900

    feat: 파일 이미지 인식 API 추가
    #71

--- src/main/java/com/groom/sumbisori/domain/file/controller/FileController.java
+++ src/main/java/com/groom/sumbisori/domain/file/controller/FileController.java
@@ -2,13 +2,18 @@ package com.groom.sumbisori.domain.file.controller;
 
 import com.groom.sumbisori.domain.file.dto.PreSignedUrlRequest;
 import com.groom.sumbisori.domain.file.dto.PreSignedUrlResponse;
+import com.groom.sumbisori.domain.file.dto.SeafoodRecognitionResponse;
+import com.groom.sumbisori.domain.file.service.ImageAnalyzeService;
 import com.groom.sumbisori.domain.file.service.S3PreSignedUrlService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,9 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FileController implements FileApi {
     private final S3PreSignedUrlService s3PreSignedUrlService;
+    private final ImageAnalyzeService imageAnalyzeService;
 
     @PostMapping("/presigned-url")
-    public List<PreSignedUrlResponse> createPreSignedUrl(@Valid @RequestBody PreSignedUrlRequest request) {
-        return s3PreSignedUrlService.create(request);
+    public ResponseEntity<List<PreSignedUrlResponse>> createPreSignedUrl(
+            @Valid @RequestBody PreSignedUrlRequest request) {
+        return ResponseEntity.ok().body(s3PreSignedUrlService.create(request));
+    }
+
+    @GetMapping("/analyze")
+    public ResponseEntity<List<SeafoodRecognitionResponse>> imageAnalyze(@RequestParam String key) {
+        return ResponseEntity.ok(imageAnalyzeService.analyze(key));
     }
 }
