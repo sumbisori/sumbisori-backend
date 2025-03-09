d408b11167df0dd081906f994a97e6db4fa1f5a2
junest66 <chlwndks333@gmail.com>
2025-03-09 21:08:52 +0900

    feat: 이미지 인식 관련 에러 코드 추가
    #71

--- src/main/java/com/groom/sumbisori/domain/file/error/FileErrorcode.java
+++ src/main/java/com/groom/sumbisori/domain/file/error/FileErrorcode.java
@@ -8,12 +8,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum FileErrorcode implements ErrorCode {
-    INVALID_IMAGE_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "허용되지 않은 이미지 파일 형식입니다.");
+    INVALID_IMAGE_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "허용되지 않은 이미지 파일 형식입니다."),
+    INVALID_FILE(HttpStatus.BAD_REQUEST, "유효하지 않은 파일입니다."),
+    S3_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "S3 서비스 오류입니다."),
+    CHAT_GPT_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "Chat GPT 서비스 오류입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
 
     public static class Const {
         public static final String INVALID_IMAGE_CONTENT_TYPE = "INVALID_IMAGE_CONTENT_TYPE";
+        public static final String INVALID_FILE = "INVALID_FILE";
+        public static final String S3_ERROR = "S3_ERROR";
+        public static final String CHAT_GPT_ERROR = "CHAT_GPT_ERROR";
     }
 }
