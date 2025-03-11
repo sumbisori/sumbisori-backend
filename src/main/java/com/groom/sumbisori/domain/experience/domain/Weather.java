526012ba13ccffd1528f7687d8dde87b6e69fe9c
junest66 <chlwndks333@gmail.com>
2025-03-12 00:38:26 +0900

    feat: 체험 Entity에 사용하는 Weather Enum 생성
    #33

---
+++ src/main/java/com/groom/sumbisori/domain/experience/domain/Weather.java
@@ -0,0 +1,5 @@
+package com.groom.sumbisori.domain.experience.domain;
+
+public enum Weather {
+    CLEAR_SKY, FEW_CLOUDS, SCATTERED_CLOUDS, BROKEN_CLOUDS, SHOWER_RAIN, RAIN, THUNDERSTORM, SNOW, MIST,
+}
