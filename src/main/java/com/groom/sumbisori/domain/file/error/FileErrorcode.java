51a7d8416e7278b2f859830a0bfb05786a68d1e9
junest66 <chlwndks333@gmail.com>
2025-03-12 00:55:47 +0900

    feat: 파일 에러 상수 추가
    #33

--- src/main/java/com/groom/sumbisori/domain/file/error/FileErrorcode.java
+++ src/main/java/com/groom/sumbisori/domain/file/error/FileErrorcode.java
@@ -10,8 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum FileErrorcode implements ErrorCode {
     INVALID_IMAGE_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "허용되지 않은 이미지 파일 형식입니다."),
     INVALID_FILE(HttpStatus.BAD_REQUEST, "유효하지 않은 파일입니다."),
-    S3_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "S3 서비스 오류입니다."),
-    CHAT_GPT_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "Chat GPT 서비스 오류입니다.");
+    INVALID_SEQUENCE(HttpStatus.BAD_REQUEST, "유효하지 않은 순서입니다."),
+    CHAT_GPT_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "Chat GPT 서비스 오류입니다."),
+    S3_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "S3 서비스 오류입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
@@ -19,6 +20,7 @@ public enum FileErrorcode implements ErrorCode {
     public static class Const {
         public static final String INVALID_IMAGE_CONTENT_TYPE = "INVALID_IMAGE_CONTENT_TYPE";
         public static final String INVALID_FILE = "INVALID_FILE";
+        public static final String INVALID_SEQUENCE = "INVALID_SEQUENCE";
         public static final String S3_ERROR = "S3_ERROR";
         public static final String CHAT_GPT_ERROR = "CHAT_GPT_ERROR";
     }
