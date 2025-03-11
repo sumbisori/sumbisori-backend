720c01f76fdda84398e47543fff89d081b0b9844
junest66 <chlwndks333@gmail.com>
2025-03-12 00:55:24 +0900

    feat: 체험 생성 서비스 함수 추가
    #33

---
+++ src/main/java/com/groom/sumbisori/domain/experience/service/ExperienceCreateService.java
@@ -0,0 +1,55 @@
+package com.groom.sumbisori.domain.experience.service;
+
+import com.groom.sumbisori.domain.collection.service.SeafoodCollectionCreateService;
+import com.groom.sumbisori.domain.experience.domain.Experience;
+import com.groom.sumbisori.domain.experience.dto.request.ExperienceRequest;
+import com.groom.sumbisori.domain.experience.dto.response.ExperienceResponse;
+import com.groom.sumbisori.domain.experience.error.ExperienceErrorcode;
+import com.groom.sumbisori.domain.experience.error.ExperienceException;
+import com.groom.sumbisori.domain.experience.repository.ExperienceRepository;
+import com.groom.sumbisori.domain.file.service.FileImageCreateService;
+import com.groom.sumbisori.domain.place.entity.Place;
+import com.groom.sumbisori.domain.place.error.PlaceErrorcode;
+import com.groom.sumbisori.domain.place.error.exception.PlaceException;
+import com.groom.sumbisori.domain.place.repository.PlaceRepository;
+import jakarta.transaction.Transactional;
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ExperienceCreateService {
+    private final PlaceRepository placeRepository;
+    private final ExperienceRepository experienceRepository;
+    private final FileImageCreateService fileImageCreateService;
+    private final SeafoodCollectionCreateService seafoodCollectionCreateService;
+
+    @Transactional
+    public ExperienceResponse create(Long userId, ExperienceRequest experienceRequest) {
+        // 1. 검사
+        validate(experienceRequest);
+
+        // 2. 체험 생성
+        Place place = placeRepository.findById(experienceRequest.placeId())
+                .orElseThrow(() -> new PlaceException(PlaceErrorcode.PLACE_NOT_FOUND));
+        Experience experience = experienceRequest.of(userId, place, experienceRequest);
+        experienceRepository.save(experience);
+        Long experienceId = experience.getId();
+
+        // 3. 체험 이미지 생성
+        fileImageCreateService.create(experienceRequest.files(), experienceId);
+
+        // 4. 수집 생성
+        seafoodCollectionCreateService.create(userId, experienceRequest.collections(), experienceId);
+        return new ExperienceResponse(experienceId);
+    }
+
+    private void validate(ExperienceRequest experienceRequest) {
+        if (experienceRequest.experienceDate().isAfter(LocalDate.now())) {
+            throw new ExperienceException(ExperienceErrorcode.EXPERIENCE_DATE_INVALID);
+        }
+    }
+}
