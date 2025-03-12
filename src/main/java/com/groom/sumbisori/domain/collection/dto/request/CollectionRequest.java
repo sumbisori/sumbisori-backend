6e2a0bd099914bb82d3b15bacfee9668906b7f0f
junest66 <chlwndks333@gmail.com>
2025-03-13 01:35:37 +0900

    refactor: 최소 해산물 수집 갯수 설정
    #33

--- src/main/java/com/groom/sumbisori/domain/collection/dto/request/CollectionRequest.java
+++ src/main/java/com/groom/sumbisori/domain/collection/dto/request/CollectionRequest.java
@@ -8,6 +8,6 @@ public record CollectionRequest(
         @NotNull(message = "파일의 imageIdentifier는 필수입니다.")
         String imageIdentifier,
 
-        @Size(max = 18, message = "최대 18개의 해산물을 요청할 수 있습니다.")
+        @Size(min = 1, max = 18, message = "최소 1개에서 최대 18개의 해산물을 요청할 수 있습니다.")
         List<CollectionInfo> collectionInfos) {
 }
