cbc9cf98e12a4fae7f5c35388f8ed638f96838b0
junest66 <chlwndks333@gmail.com>
2025-03-13 01:28:09 +0900

    refactor: 수집 함수 수정
    #33

--- src/main/java/com/groom/sumbisori/domain/collection/service/SeafoodCollectionCreateService.java
+++ src/main/java/com/groom/sumbisori/domain/collection/service/SeafoodCollectionCreateService.java
@@ -3,10 +3,9 @@ package com.groom.sumbisori.domain.collection.service;
 import com.groom.sumbisori.domain.collection.dto.request.CollectionRequest;
 import com.groom.sumbisori.domain.collection.entity.SeafoodCollection;
 import com.groom.sumbisori.domain.collection.repository.SeafoodCollectionRepository;
+import com.groom.sumbisori.domain.collectionitem.service.CollectionItemCreateService;
 import com.groom.sumbisori.domain.file.service.FileCopyService;
-import com.groom.sumbisori.domain.seafood.error.SeafoodErrorCode;
-import com.groom.sumbisori.domain.seafood.error.exception.SeafoodException;
-import com.groom.sumbisori.domain.seafood.repository.SeafoodRepository;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,24 +14,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SeafoodCollectionCreateService {
     private final FileCopyService fileCopyService;
-    private final SeafoodRepository seafoodRepository;
+    private final CollectionItemCreateService collectionItemCreateService;
     private final SeafoodCollectionRepository seafoodCollectionRepository;
 
+    @Transactional
     public void create(Long userId, List<CollectionRequest> seafoodCollectionRequests, Long experienceId) {
-        List<SeafoodCollection> seafoodCollections = seafoodCollectionRequests.stream()
-                .map(c -> {
-                    fileCopyService.copy(c.imageIdentifier());
-                    return SeafoodCollection.builder()
-                            .userId(userId)
-                            .experienceId(experienceId)
-                            .imageIdentifier(c.imageIdentifier())
-                            .seafood(seafoodRepository.findById(c.seafoodId())
-                                    .orElseThrow(() -> new SeafoodException(SeafoodErrorCode.SEAFOOD_NOT_FOUND)))
-                            .quantity(c.quantity())
-                            .build();
-                })
-                .toList();
-
-        seafoodCollectionRepository.saveAll(seafoodCollections);
+        for (CollectionRequest request : seafoodCollectionRequests) {
+            fileCopyService.copy(request.imageIdentifier());
+            SeafoodCollection seafoodCollection = SeafoodCollection.builder()
+                    .experienceId(experienceId)
+                    .imageIdentifier(request.imageIdentifier())
+                    .build();
+            seafoodCollectionRepository.save(seafoodCollection);
+            collectionItemCreateService.create(userId, seafoodCollection.getId(), request.collectionInfos());
+        }
     }
 }
