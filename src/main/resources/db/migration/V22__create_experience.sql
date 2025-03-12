b507f5115a8b7e379d1bff231f169733bd2d1e26
junest66 <chlwndks333@gmail.com>
2025-03-13 01:29:09 +0900

    refactor: flyway 스크립트 변경
    #33

--- src/main/resources/db/migration/V22__create_experience.sql
+++ src/main/resources/db/migration/V22__create_experience.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS seafood_collection;
+
 CREATE TABLE experience
 (
     experience_id   BIGINT AUTO_INCREMENT NOT NULL,
@@ -24,25 +26,42 @@ CREATE TABLE file
     CONSTRAINT pk_file PRIMARY KEY (file_id)
 );
 
-DELETE
-FROM seafood_collection;
+CREATE TABLE seafood_collection
+(
+    seafood_collection_id BIGINT AUTO_INCREMENT NOT NULL,
+    created_at            datetime              NULL,
+    updated_at            datetime              NULL,
+    experience_id         BIGINT                NOT NULL,
+    image_identifier      VARCHAR(255)          NOT NULL,
+    CONSTRAINT pk_seafood_collection PRIMARY KEY (seafood_collection_id)
+);
 
-ALTER TABLE seafood_collection
-    AUTO_INCREMENT = 1;
-
-ALTER TABLE seafood_collection
-    ADD experience_id BIGINT NOT NULL;
-
-ALTER TABLE seafood_collection
-    ADD image_identifier VARCHAR(255) NOT NULL;
+CREATE TABLE seafood_collection_item
+(
+    collection_item_id    BIGINT AUTO_INCREMENT NOT NULL,
+    created_at            datetime              NULL,
+    updated_at            datetime              NULL,
+    user_id               BIGINT                NOT NULL,
+    seafood_collection_id BIGINT                NOT NULL,
+    seafood_id            BIGINT                NOT NULL,
+    quantity              INT                   NOT NULL,
+    CONSTRAINT pk_seafood_collection_item PRIMARY KEY (collection_item_id)
+);
 
 CREATE INDEX idx_experience_user_id ON experience (user_id);
 
 CREATE INDEX idx_file_experience_sequence ON file (experience_id, sequence, image_identifier);
 
+CREATE INDEX idx_seafood_collection_covering ON seafood_collection_item (seafood_collection_id, seafood_id, quantity);
+
 CREATE INDEX idx_seafood_collection_experience ON seafood_collection (experience_id);
 
-CREATE INDEX idx_seafood_collection_user ON seafood_collection (user_id);
+CREATE INDEX idx_user_covering ON seafood_collection_item (user_id, seafood_id, quantity);
 
 ALTER TABLE experience
     ADD CONSTRAINT FK_EXPERIENCE_ON_PLACE FOREIGN KEY (place_id) REFERENCES place (place_id);
+
+ALTER TABLE seafood_collection_item
+    ADD CONSTRAINT FK_SEAFOOD_COLLECTION_ITEM_ON_SEAFOOD FOREIGN KEY (seafood_id) REFERENCES seafood (seafood_id);
+
+DROP TABLE IF EXISTS reservation;
