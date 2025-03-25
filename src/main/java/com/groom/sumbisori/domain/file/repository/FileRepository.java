8a58d729c7fa7fb91e2c1211b31d71ff7edb2281
junest66 <chlwndks333@gmail.com>
2025-03-26 03:05:37 +0900

    feat: 파일 조회 쿼리 추가
    #76

--- src/main/java/com/groom/sumbisori/domain/file/repository/FileRepository.java
+++ src/main/java/com/groom/sumbisori/domain/file/repository/FileRepository.java
@@ -1,9 +1,11 @@
 package com.groom.sumbisori.domain.file.repository;
 
 import com.groom.sumbisori.domain.file.entity.File;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
+    Optional<File> findByImageIdentifier(String imageIdentifier);
 }
