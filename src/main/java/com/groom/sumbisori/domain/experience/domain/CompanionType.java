e96603043a5832df6140a44e60337ea9f70f8e86
junest66 <chlwndks333@gmail.com>
2025-03-26 02:51:52 +0900

    refactor: enum 값에 한국이름 추가
    #76

--- src/main/java/com/groom/sumbisori/domain/experience/domain/CompanionType.java
+++ src/main/java/com/groom/sumbisori/domain/experience/domain/CompanionType.java
@@ -1,8 +1,17 @@
 package com.groom.sumbisori.domain.experience.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum CompanionType {
-    ALONE, FRIEND, LOVER, FAMILY, COLLEAGUE, RELATIVE;
+    ALONE("혼자"),
+    FRIEND("친구"),
+    LOVER("연인"),
+    FAMILY("가족"),
+    COLLEAGUE("동료"),
+    RELATIVE("친척");
+
+    private final String label;
 }
--- src/main/java/com/groom/sumbisori/domain/experience/domain/Weather.java
+++ src/main/java/com/groom/sumbisori/domain/experience/domain/Weather.java
@@ -1,5 +1,20 @@
 package com.groom.sumbisori.domain.experience.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Weather {
-    CLEAR_SKY, FEW_CLOUDS, SCATTERED_CLOUDS, BROKEN_CLOUDS, SHOWER_RAIN, RAIN, THUNDERSTORM, SNOW, MIST,
+    CLEAR_SKY("맑음"),
+    FEW_CLOUDS("구름 조금"),
+    SCATTERED_CLOUDS("흩어진 구름"),
+    BROKEN_CLOUDS("구름 많음"),
+    SHOWER_RAIN("소나기"),
+    RAIN("비"),
+    THUNDERSTORM("뇌우"),
+    SNOW("눈"),
+    MIST("안개");
+
+    private final String label;
 }
