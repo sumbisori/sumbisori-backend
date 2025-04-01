351e3817aa01c96aee90e806dffcc793608f112c
junest66 <chlwndks333@gmail.com>
2025-04-01 19:40:11 +0900

    fix: 체험 권한 에러 코드 추가
    #80

--- src/main/java/com/groom/sumbisori/domain/experience/error/ExperienceErrorcode.java
+++ src/main/java/com/groom/sumbisori/domain/experience/error/ExperienceErrorcode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ExperienceErrorcode implements ErrorCode {
     EXPERIENCE_DATE_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 체 날짜입니다."),
+    EXPERIENCE_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "체험에 대한 권한이 없습니다."),
     EXPERIENCE_NOT_FOUND(HttpStatus.NOT_FOUND, "체험을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
@@ -16,6 +17,7 @@ public enum ExperienceErrorcode implements ErrorCode {
 
     public static class Const {
         public static final String EXPERIENCE_DATE_INVALID = "EXPERIENCE_DATE_INVALID";
+        public static final String EXPERIENCE_PERMISSION_DENIED = "EXPERIENCE_PERMISSION_DENIED";
         public static final String EXPERIENCE_NOT_FOUND = "EXPERIENCE_NOT_FOUND";
     }
 
