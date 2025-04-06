2557ae15fe21634740f2fcb334a4a47d61fc2f91
junest66 <chlwndks333@gmail.com>
2025-04-06 14:23:07 +0900

    feat: 닉네임 조회 함수 쿼리 추가
    #89

--- src/main/java/com/groom/sumbisori/domain/user/repository/UserRepository.java
+++ src/main/java/com/groom/sumbisori/domain/user/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.groom.sumbisori.domain.user.repository;
 import com.groom.sumbisori.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByProviderIdAndProviderType(String providerId, String providerType);
 
-    Optional<User> findByNickname(String nickname);
+    @Query("SELECT u.nickname FROM User u WHERE u.id = :userId")
+    String findNicknameById(@Param("userId") Long userId);
 }
