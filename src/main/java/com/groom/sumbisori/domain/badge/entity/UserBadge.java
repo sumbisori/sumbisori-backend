48c33b070bdd9fec86db0b1a4d861595323276b3
junest66 <chlwndks333@gmail.com>
2025-04-23 03:49:52 +0900

    refactor: UserBadge Entity 구조 변경
    #92

--- src/main/java/com/groom/sumbisori/domain/badge/entity/UserBadge.java
+++ src/main/java/com/groom/sumbisori/domain/badge/entity/UserBadge.java
@@ -3,16 +3,14 @@ package com.groom.sumbisori.domain.badge.entity;
 import com.groom.sumbisori.domain.base.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,12 +22,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(
-        name = "user_badge",
-        indexes = {
-                @Index(name = "idx_user_badge_user_id", columnList = "user_id")
-        }
-)
+@Table(name = "user_badge",
+        uniqueConstraints = @UniqueConstraint(name = "unique_user_badge", columnNames = {"userId", "badge_level_id"}))
+
 public class UserBadge extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,10 +35,6 @@ public class UserBadge extends BaseTimeEntity {
     private Long userId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "badge_id")
-    private Badge badge;
-
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private BadgeLevel level;
+    @JoinColumn(name = "badge_level_id", nullable = false)
+    private BadgeLevel badgeLevel;
 }
