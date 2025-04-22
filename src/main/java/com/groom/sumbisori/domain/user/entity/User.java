fc5ec70208351392d0f5b7d5fe44dee9c5e8c6eb
junest66 <chlwndks333@gmail.com>
2025-04-23 03:50:43 +0900

    feat: 사용자의 대표 뱃지 레벨 추가
    #92

--- src/main/java/com/groom/sumbisori/domain/user/entity/User.java
+++ src/main/java/com/groom/sumbisori/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.groom.sumbisori.domain.user.entity;
 
+import com.groom.sumbisori.domain.badge.entity.BadgeLevel;
 import com.groom.sumbisori.domain.base.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,6 +9,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDateTime;
@@ -52,8 +55,9 @@ public class User extends BaseTimeEntity {
     @Column
     private LocalDateTime lastLoginAt;
 
-    @Column
-    private Long representativeBadgeId;
+    @ManyToOne
+    @JoinColumn(name = "badge_level_id")
+    private BadgeLevel badgeLevel;
 
     public void update(String email, String nickname, String profileImageUrl) {
         this.email = email;
