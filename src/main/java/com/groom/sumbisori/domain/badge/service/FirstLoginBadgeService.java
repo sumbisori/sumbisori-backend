ec38afcb7f181ef508164cbd21a12f952dc14e5d
junest66 <chlwndks333@gmail.com>
2025-04-23 04:02:28 +0900

    feat: 첫 로그인 시 회원가입 배지를 부여하는 로직 설정
    #92

--- src/main/java/com/groom/sumbisori/domain/badge/service/FirstLoginBadgeService.java
+++ src/main/java/com/groom/sumbisori/domain/badge/service/FirstLoginBadgeService.java
@@ -1,9 +1,14 @@
 package com.groom.sumbisori.domain.badge.service;
 
 import com.groom.sumbisori.domain.badge.dto.event.SignUpEvent;
-import com.groom.sumbisori.domain.badge.entity.Badge;
-import com.groom.sumbisori.domain.badge.entity.BadgeType;
-import com.groom.sumbisori.domain.badge.repository.BadgeRepository;
+import com.groom.sumbisori.domain.badge.entity.BadgeCode;
+import com.groom.sumbisori.domain.badge.entity.BadgeLevel;
+import com.groom.sumbisori.domain.badge.entity.UserBadge;
+import com.groom.sumbisori.domain.badge.error.BadgeErrorcode;
+import com.groom.sumbisori.domain.badge.error.BadgeException;
+import com.groom.sumbisori.domain.badge.repository.BadgeLevelRepository;
+import com.groom.sumbisori.domain.badge.repository.UserBadgeRepository;
+import com.groom.sumbisori.domain.user.service.UserBadgeUpdateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -17,7 +22,9 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Slf4j
 @Transactional
 public class FirstLoginBadgeService {
-    private final BadgeRepository badgeRepository;
+    private final UserBadgeUpdateService userBadgeUpdateService;
+    private final BadgeLevelRepository badgeLevelRepository;
+    private final UserBadgeRepository userBadgeRepository;
 
     /**
      * 첫 로그인 시 뱃지를 부여
@@ -25,11 +32,15 @@ public class FirstLoginBadgeService {
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void awardFirstJoinBadge(SignUpEvent event) {
-        log.info("FirstLoginBadgeService.awardFirstJoinBadge() - userId: {}", event.userId());
-//        Badge badge = Badge.builder()
-//                .userId(event.userId())
-//                .type(BadgeType.FIRST_LOGIN)
-//                .build();
-//        badgeRepository.save(badge);
+        BadgeLevel badgeLevel = badgeLevelRepository.findByCodeAndLevel(BadgeCode.FIRST_JOIN, 1)
+                .orElseThrow(() -> new BadgeException(BadgeErrorcode.BADGE_NOT_FOUND));
+
+        UserBadge userBadge = UserBadge.builder()
+                .userId(event.userId())
+                .badgeLevel(badgeLevel)
+                .build();
+        userBadgeRepository.save(userBadge);
+        userBadgeUpdateService.updateUserBadgeLevel(event.userId(), badgeLevel);
+        log.info("첫 로그인 뱃지 부여 - 사용자 ID: {}", event.userId());
     }
 }
