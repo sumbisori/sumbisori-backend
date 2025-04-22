5b451939b44621fb34dbfd1c595d49bd0f1979f4
junest66 <chlwndks333@gmail.com>
2025-04-23 03:51:36 +0900

    feat: 배지 조회 API 추가
    #92

--- src/main/java/com/groom/sumbisori/domain/badge/controller/BadgeApi.java
+++ src/main/java/com/groom/sumbisori/domain/badge/controller/BadgeApi.java
@@ -1,14 +1,19 @@
 package com.groom.sumbisori.domain.badge.controller;
 
 import com.groom.sumbisori.common.config.LoginUser;
-import com.groom.sumbisori.domain.badge.dto.response.MyBadgeStatus;
+import com.groom.sumbisori.domain.badge.dto.response.BadgeDetail;
+import com.groom.sumbisori.domain.badge.dto.response.BadgeStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Tag(name = "badges", description = "배지 API")
 public interface BadgeApi {
     @Operation(summary = "배지 현황 조회")
-    ResponseEntity<List<MyBadgeStatus>> getMyBadgeStatuses(@LoginUser final Long userId);
+    ResponseEntity<List<BadgeStatus>> getMyBadgeStatuses(@LoginUser final Long userId);
+
+    @Operation(summary = "단일 배지 조회")
+    ResponseEntity<BadgeDetail> getBadgeDetail(@LoginUser final Long userId, @PathVariable final Long badgeId);
 }
--- src/main/java/com/groom/sumbisori/domain/badge/controller/BadgeController.java
+++ src/main/java/com/groom/sumbisori/domain/badge/controller/BadgeController.java
@@ -1,12 +1,14 @@
 package com.groom.sumbisori.domain.badge.controller;
 
 import com.groom.sumbisori.common.config.LoginUser;
-import com.groom.sumbisori.domain.badge.dto.response.MyBadgeStatus;
+import com.groom.sumbisori.domain.badge.dto.response.BadgeDetail;
+import com.groom.sumbisori.domain.badge.dto.response.BadgeStatus;
 import com.groom.sumbisori.domain.badge.service.BadgeLookupService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,13 @@ public class BadgeController implements BadgeApi {
 
     @Override
     @GetMapping
-    public ResponseEntity<List<MyBadgeStatus>> getMyBadgeStatuses(@LoginUser final Long userId) {
-        return ResponseEntity.ok(badgeLookupService.getMyBadgeStatuses(userId));
+    public ResponseEntity<List<BadgeStatus>> getMyBadgeStatuses(@LoginUser final Long userId) {
+        return ResponseEntity.ok(badgeLookupService.getBadgeStatuses(userId));
+    }
+
+    @Override
+    @GetMapping("/{badgeId}")
+    public ResponseEntity<BadgeDetail> getBadgeDetail(@LoginUser Long userId, @PathVariable Long badgeId) {
+        return ResponseEntity.ok(badgeLookupService.getBadgeDetail(userId, badgeId));
     }
 }
