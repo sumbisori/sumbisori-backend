79ec9b36c73c2d28c2738e5355621c97b68c92c7
junest66 <chlwndks333@gmail.com>
2025-04-23 05:49:48 +0900

    refactor: 회원 프로필 조회에 배지 관련 정보 추가
    #91

--- src/main/java/com/groom/sumbisori/domain/user/service/UserLookupService.java
+++ src/main/java/com/groom/sumbisori/domain/user/service/UserLookupService.java
@@ -1,11 +1,12 @@
 package com.groom.sumbisori.domain.user.service;
 
-import com.groom.sumbisori.domain.collection.repository.CollectionQueryRepository;
+import com.groom.sumbisori.domain.badge.entity.BadgeLevel;
 import com.groom.sumbisori.domain.user.dto.response.UserNickname;
 import com.groom.sumbisori.domain.user.dto.response.UserProfile;
 import com.groom.sumbisori.domain.user.entity.User;
 import com.groom.sumbisori.domain.user.error.UserErrorCode;
 import com.groom.sumbisori.domain.user.error.exception.UserException;
+import com.groom.sumbisori.domain.user.repository.UserQueryRepository;
 import com.groom.sumbisori.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,18 +18,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserLookupService {
     private final UserRepository userRepository;
-    private final CollectionQueryRepository collectionQueryRepository;
+    private final UserQueryRepository userQueryRepository;
 
     @Transactional(readOnly = true)
     public UserProfile getMyInfo(Long userId) {
-        User user = userRepository.findById(userId)
+        return userQueryRepository.findUserProfile(userId)
                 .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
-        int count = collectionQueryRepository.sumQuantityByUserId(userId);
-        return UserProfile.builder()
-                .count(count)
-                .profileImageUrl(user.getProfileImageUrl())
-                .nickname(user.getNickname())
-                .build();
     }
 
     @Transactional(readOnly = true)
