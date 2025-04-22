24dc692d6f262e70e9f0167b72f956668ade0d8e
junest66 <chlwndks333@gmail.com>
2025-04-23 04:46:29 +0900

    fix: null 처리 버그 수정
    #92

--- src/main/java/com/groom/sumbisori/domain/badge/service/BadgeLookupService.java
+++ src/main/java/com/groom/sumbisori/domain/badge/service/BadgeLookupService.java
@@ -3,15 +3,18 @@ package com.groom.sumbisori.domain.badge.service;
 import com.groom.sumbisori.domain.badge.dto.response.BadgeDetail;
 import com.groom.sumbisori.domain.badge.dto.response.BadgeStatus;
 import com.groom.sumbisori.domain.badge.entity.Badge;
+import com.groom.sumbisori.domain.badge.entity.BadgeLevel;
 import com.groom.sumbisori.domain.badge.error.BadgeErrorcode;
 import com.groom.sumbisori.domain.badge.error.BadgeException;
 import com.groom.sumbisori.domain.badge.repository.BadgeQueryRepository;
 import com.groom.sumbisori.domain.badge.repository.UserBadgeQueryRepository;
+import com.groom.sumbisori.domain.user.entity.User;
 import com.groom.sumbisori.domain.user.error.UserErrorCode;
 import com.groom.sumbisori.domain.user.error.exception.UserException;
 import com.groom.sumbisori.domain.user.repository.UserRepository;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,9 +39,13 @@ public class BadgeLookupService {
         List<Badge> badges = badgeCacheService.getAllBadges();
 
         // 대표 뱃지 조회
-        Long representativeBadgeId = userRepository.findById(userId)
-                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND))
-                .getBadgeLevel().getBadge().getId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+        Long representativeBadgeId = Optional.of(user)
+                .map(User::getBadgeLevel)
+                .map(BadgeLevel::getBadge)
+                .map(Badge::getId)
+                .orElse(null);
 
         return badges.stream()
                 .map(badge -> {
