bb71d8a4200219a56b1ad4da91e4a6963f78e7b7
junest66 <chlwndks333@gmail.com>
2025-05-17 01:03:18 +0900

    feat: 알람 테이블 생성 스크립트 추가
    #106

---
+++ src/main/resources/db/migration/V32__create_alarm.sql
@@ -0,0 +1,18 @@
+ALTER TABLE users
+    DROP FOREIGN KEY FK_USERS_ON_BADGE_LEVEL;
+
+CREATE TABLE alarm
+(
+    alarm_id   BIGINT AUTO_INCREMENT NOT NULL,
+    user_id    BIGINT                NULL,
+    alarm_type SMALLINT              NULL,
+    content    VARCHAR(255)          NULL,
+    link       VARCHAR(255)          NULL,
+    is_read    BIT(1)                NULL,
+    is_deleted BIT(1)                NULL,
+    created_at datetime              NULL,
+    updated_at datetime              NULL,
+    CONSTRAINT pk_alarm PRIMARY KEY (alarm_id)
+);
+
+CREATE INDEX idx_alarm_user_created ON alarm (user_id, is_deleted, created_at);
