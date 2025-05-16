e8c1d86e9a08f5018adda937c0fd40f4473aff9e
junest66 <chlwndks333@gmail.com>
2025-05-17 01:47:30 +0900

    feat: 알림 조회 API 구현
    #106

---
+++ src/main/java/com/groom/sumbisori/domain/alarm/controller/AlarmController.java
@@ -0,0 +1,39 @@
+package com.groom.sumbisori.domain.alarm.controller;
+
+import com.groom.sumbisori.common.config.LoginUser;
+import com.groom.sumbisori.domain.alarm.dto.response.AlarmResponse;
+import com.groom.sumbisori.domain.alarm.service.AlarmLookupService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alarms")
+@RequiredArgsConstructor
+public class AlarmController implements AlarmApi {
+    private final AlarmLookupService alarmLookupService;
+
+    @Override
+    @GetMapping
+    public ResponseEntity<List<AlarmResponse>> getAlarms(@LoginUser Long userId, Pageable pageable) {
+        return ResponseEntity.ok(alarmLookupService.lookup(userId));
+    }
+
+    @Override
+    @PostMapping("/{alarmId}")
+    public ResponseEntity<Void> readAlarm(Long userId, Long alarmId) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("/{alarmId}")
+    public ResponseEntity<Void> deleteAlarm(Long userId, Long alarmId) {
+        return null;
+    }
+}
