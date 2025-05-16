032e0f9da0cc421e99422c00a3e3a92a090f143a
junest66 <chlwndks333@gmail.com>
2025-05-17 01:29:49 +0900

    refactor: 누락된 Enum 설정 부분 추가
    #106

--- src/main/resources/db/migration/V32__alarm_create.sql
+++ src/main/resources/db/migration/V32__alarm_create.sql
@@ -1,12 +1,12 @@
 CREATE TABLE alarm
 (
     alarm_id   BIGINT AUTO_INCREMENT NOT NULL,
-    user_id    BIGINT                NULL,
-    alarm_type SMALLINT              NULL,
-    content    VARCHAR(255)          NULL,
+    user_id    BIGINT                NOT NULL,
+    type       VARCHAR(255)          NOT NULL,
+    content    VARCHAR(255)          NOT NULL,
     link       VARCHAR(255)          NULL,
-    is_read    BIT(1)                NULL,
-    is_deleted BIT(1)                NULL,
+    is_read    BIT(1)                NOT NULL,
+    is_deleted BIT(1)                NOT NULL,
     created_at datetime              NULL,
     updated_at datetime              NULL,
     CONSTRAINT pk_alarm PRIMARY KEY (alarm_id)
