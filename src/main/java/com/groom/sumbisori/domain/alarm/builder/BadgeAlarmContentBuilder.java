a1d9481aced985625ef65f8a0b30726e51f22d09
junest66 <chlwndks333@gmail.com>
2025-05-17 12:51:08 +0900

    feat: 배지 알람 내용 생성 로직 구현
    #108

--- src/main/java/com/groom/sumbisori/domain/alarm/builder/BadgeAlarmContentBuilder.java
+++ src/main/java/com/groom/sumbisori/domain/alarm/builder/BadgeAlarmContentBuilder.java
@@ -3,10 +3,14 @@ package com.groom.sumbisori.domain.alarm.builder;
 import com.groom.sumbisori.domain.alarm.context.BadgeAlarmContext;
 import com.groom.sumbisori.domain.alarm.dto.common.AlarmContent;
 import com.groom.sumbisori.domain.alarm.entity.AlarmType;
+import com.groom.sumbisori.domain.badge.entity.BadgeRankLevel;
+import com.groom.sumbisori.domain.badge.entity.BadgeType;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BadgeAlarmContentBuilder implements AlarmContentBuilder<BadgeAlarmContext> {
+    private final static String MESSAGE_TEMPLATE = "%s %s배지를 획득했습니다.";
+    private final static String LINK_TEMPLATE = "/my-page/badge?badge-id=%s";
 
     @Override
     public AlarmType getType() {
@@ -15,6 +19,15 @@ public class BadgeAlarmContentBuilder implements AlarmContentBuilder<BadgeAlarmC
 
     @Override
     public AlarmContent build(BadgeAlarmContext ctx) {
-        return null;
+        String message = String.format(MESSAGE_TEMPLATE, ctx.name(), getBadgeLevelLabel(ctx.badgeType(), ctx.level()));
+        String link = String.format(LINK_TEMPLATE, ctx.badgeId());
+        return AlarmContent.of(message, link);
+    }
+
+    private String getBadgeLevelLabel(BadgeType badgeType, int level) {
+        if (badgeType == BadgeType.RANKED) {
+            return BadgeRankLevel.getLabel(level);
+        }
+        return "";
     }
 }
