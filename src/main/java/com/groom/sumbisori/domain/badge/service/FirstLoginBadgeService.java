526da247c5c237d39b37948d081d93938224b1f2
junest66 <chlwndks333@gmail.com>
2025-05-17 14:14:33 +0900

    refactor: 첫 로그인 시 배지 발급 코드 적용
    #108

--- src/main/java/com/groom/sumbisori/domain/badge/service/FirstLoginBadgeService.java
+++ src/main/java/com/groom/sumbisori/domain/badge/service/FirstLoginBadgeService.java
@@ -3,11 +3,9 @@ package com.groom.sumbisori.domain.badge.service;
 import com.groom.sumbisori.domain.badge.dto.event.SignUpEvent;
 import com.groom.sumbisori.domain.badge.entity.BadgeCode;
 import com.groom.sumbisori.domain.badge.entity.BadgeLevel;
-import com.groom.sumbisori.domain.badge.entity.UserBadge;
 import com.groom.sumbisori.domain.badge.error.BadgeErrorcode;
 import com.groom.sumbisori.domain.badge.error.BadgeException;
-import com.groom.sumbisori.domain.badge.repository.BadgeLevelRepository;
-import com.groom.sumbisori.domain.badge.repository.UserBadgeRepository;
+import com.groom.sumbisori.domain.badge.repository.BadgeLevelQueryRepository;
 import com.groom.sumbisori.domain.user.service.UserBadgeUpdateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,8 +21,8 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Transactional
 public class FirstLoginBadgeService {
     private final UserBadgeUpdateService userBadgeUpdateService;
-    private final BadgeLevelRepository badgeLevelRepository;
-    private final UserBadgeRepository userBadgeRepository;
+    private final BadgeCreateService badgeCreateService;
+    private final BadgeLevelQueryRepository badgeLevelQueryRepository;
 
     /**
      * 첫 로그인 시 뱃지를 부여
@@ -32,14 +30,9 @@ public class FirstLoginBadgeService {
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void awardFirstJoinBadge(SignUpEvent event) {
-        BadgeLevel badgeLevel = badgeLevelRepository.findByCodeAndLevel(BadgeCode.FIRST_JOIN, 1)
+        BadgeLevel badgeLevel = badgeLevelQueryRepository.findByCodeAndLevelFetchBadge(BadgeCode.FIRST_JOIN, 1)
                 .orElseThrow(() -> new BadgeException(BadgeErrorcode.BADGE_NOT_FOUND));
-
-        UserBadge userBadge = UserBadge.builder()
-                .userId(event.userId())
-                .badgeLevel(badgeLevel)
-                .build();
-        userBadgeRepository.save(userBadge);
+        badgeCreateService.create(event.userId(), badgeLevel);
         userBadgeUpdateService.updateUserBadgeLevel(event.userId(), badgeLevel.getId());
         log.info("첫 로그인 뱃지 부여 - 사용자 ID: {}", event.userId());
     }
