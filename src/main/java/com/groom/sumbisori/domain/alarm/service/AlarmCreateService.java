3eb92fc07704288af87cfb41981af333b2c65f8a
junest66 <chlwndks333@gmail.com>
2025-05-17 12:33:15 +0900

    feat: 알림 저장 서비스 함수 추가
    #108

---
+++ src/main/java/com/groom/sumbisori/domain/alarm/service/AlarmCreateService.java
@@ -0,0 +1,26 @@
+package com.groom.sumbisori.domain.alarm.service;
+
+import com.groom.sumbisori.domain.alarm.context.AlarmContext;
+import com.groom.sumbisori.domain.alarm.dto.common.AlarmContent;
+import com.groom.sumbisori.domain.alarm.entity.Alarm;
+import com.groom.sumbisori.domain.alarm.entity.AlarmType;
+import com.groom.sumbisori.domain.alarm.registry.AlarmContentBuilderRegistry;
+import com.groom.sumbisori.domain.alarm.repository.AlarmRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AlarmCreateService {
+    private final AlarmRepository alarmRepository;
+    private final AlarmContentBuilderRegistry alarmContentBuilderRegistry;
+
+    public void create(Long userId, AlarmType type, AlarmContext context) {
+        AlarmContent content = alarmContentBuilderRegistry.get(type).build(context);
+        Alarm alarm = Alarm.create(userId, type, content);
+        alarmRepository.save(alarm);
+        // alarmWebSocketService.sendAlarm(userId, alarm); // 실시간 전송
+    }
+}
