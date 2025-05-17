5b36cea57a9672b1af1fcc8a23ff25790d7e7e48
junest66 <chlwndks333@gmail.com>
2025-05-17 13:11:30 +0900

    refactor: 배지 타입에 따라 메세지 구조 변경
    #108

--- src/main/java/com/groom/sumbisori/domain/alarm/builder/BadgeAlarmContentBuilder.java
+++ src/main/java/com/groom/sumbisori/domain/alarm/builder/BadgeAlarmContentBuilder.java
@@ -4,12 +4,12 @@ import com.groom.sumbisori.domain.alarm.context.BadgeAlarmContext;
 import com.groom.sumbisori.domain.alarm.dto.common.AlarmContent;
 import com.groom.sumbisori.domain.alarm.entity.AlarmType;
 import com.groom.sumbisori.domain.badge.entity.BadgeRankLevel;
-import com.groom.sumbisori.domain.badge.entity.BadgeType;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BadgeAlarmContentBuilder implements AlarmContentBuilder<BadgeAlarmContext> {
-    private final static String MESSAGE_TEMPLATE = "%s %s배지를 획득했습니다.";
+    private static final String MESSAGE_TEMPLATE_COMMON = "%s 배지를 획득했습니다.";
+    private static final String MESSAGE_TEMPLATE_RANKED = "%s %s배지를 획득했습니다.";
     private final static String LINK_TEMPLATE = "/my-page/badge?badge-id=%s";
 
     @Override
@@ -19,15 +19,15 @@ public class BadgeAlarmContentBuilder implements AlarmContentBuilder<BadgeAlarmC
 
     @Override
     public AlarmContent build(BadgeAlarmContext ctx) {
-        String message = String.format(MESSAGE_TEMPLATE, ctx.name(), getBadgeLevelLabel(ctx.badgeType(), ctx.level()));
+        String message = switch (ctx.badgeType()) {
+            case BASIC, SPECIAL -> String.format(MESSAGE_TEMPLATE_COMMON, ctx.name());
+            case RANKED -> String.format(
+                    MESSAGE_TEMPLATE_RANKED,
+                    ctx.name(),
+                    BadgeRankLevel.getLabel(ctx.level())
+            );
+        };
         String link = String.format(LINK_TEMPLATE, ctx.badgeId());
         return AlarmContent.of(message, link);
-    }
-
-    private String getBadgeLevelLabel(BadgeType badgeType, int level) {
-        if (badgeType == BadgeType.RANKED) {
-            return BadgeRankLevel.getLabel(level);
-        }
-        return "";
     }
 }
