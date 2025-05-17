1e4817e6d23af17f9963d1ddddc087eba2a000fe
junest66 <chlwndks333@gmail.com>
2025-05-17 14:12:49 +0900

    feat: 배지 생성 구조 변경
    - 하나의 메서드에서 배지를 생성해서 배지 생성 이벤트 발행
    
    #108

---
+++ src/main/java/com/groom/sumbisori/domain/badge/service/BadgeCreateService.java
@@ -0,0 +1,26 @@
+package com.groom.sumbisori.domain.badge.service;
+
+import com.groom.sumbisori.domain.badge.dto.event.BadgeIssuedEvent;
+import com.groom.sumbisori.domain.badge.entity.BadgeLevel;
+import com.groom.sumbisori.domain.badge.entity.UserBadge;
+import com.groom.sumbisori.domain.badge.repository.UserBadgeRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BadgeCreateService {
+    private final UserBadgeRepository userBadgeRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    public void create(Long userId, BadgeLevel badgeLevel) {
+        boolean alreadyHas = userBadgeRepository.existsByUserIdAndBadgeLevelId(userId, badgeLevel.getId());
+        if (!alreadyHas) {
+            userBadgeRepository.save(UserBadge.create(userId, badgeLevel));
+            eventPublisher.publishEvent(BadgeIssuedEvent.of(userId, badgeLevel.getBadge(), badgeLevel));
+        }
+    }
+}
