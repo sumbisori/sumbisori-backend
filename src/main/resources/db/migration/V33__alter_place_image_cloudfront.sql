91484b8885dbe20b81ef0d3cc89ebcc3158d88d1
junest66 <chlwndks333@gmail.com>
2025-06-27 03:10:37 +0900

    refactor: 파일명 오타 수정

--- src/main/resources/db/migration/V33__alter_place_image_cloudfront.sql
+++ src/main/resources/db/migration/V33__alter_place_image_cloudfront.sql
@@ -1,3 +1,7 @@
 UPDATE place
 SET image_url = REPLACE(image_url, 'https://d35m5peyvblzpd.cloudfront.net', '')
 WHERE image_url LIKE 'https://d35m5peyvblzpd.cloudfront.net/%';
+
+UPDATE icon
+SET url = REPLACE(url, 'https://d35m5peyvblzpd.cloudfront.net', '')
+WHERE url LIKE 'https://d35m5peyvblzpd.cloudfront.net/%';
--- src/main/resources/db/migration/V34_alter_icon_url.sql
+++
@@ -1,3 +0,0 @@
-UPDATE icon
-SET url = REPLACE(url, 'https://d35m5peyvblzpd.cloudfront.net', '')
-WHERE url LIKE 'https://d35m5peyvblzpd.cloudfront.net/%';
